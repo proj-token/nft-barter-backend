9f086c3621e0602f01bc09ef4ce64761d957d215
HassanAmed <hassan4621@gmail.com>
2022-10-11 18:03:04 +0500

    update seed info

--- src/modules/collection/seed.ts
+++ src/modules/collection/seed.ts
@@ -3,37 +3,28 @@ export const collections = [
   {
     title: 'Beanz',
     type: 'ERC721',
-    imgUrl: 'img/art-work/c1.png',
+    imgUrl:
+      'https://i.seadn.io/gae/_R4fuC4QGYd14-KwX2bD1wf-AWjDF2VMabfqWFJhIgiN2FnAUpnD5PLdJORrhQ8gly7KcjhQZZpuzYVPF7CDSzsqmDh97z84j2On?auto=format&w=3840',
     address: '0xb4f323cfA8B2c398731b1E32A9299018449079D9',
-  },
-  {
-    title: 'Doodle',
-    type: 'ERC721',
-    imgUrl: 'img/art-work/c1.png',
-    address: '0xE2D8312c7398D65bDf29937EA98cB6Acb7195599',
   },
   {
     title: 'Azuki Copy',
     type: 'ERC721',
-    imgUrl: 'img/art-work/c1.png',
+    imgUrl:
+      'https://i.seadn.io/gae/H8jOCJuQokNqGBpkBN5wk1oZwO7LM8bNnrHCaekV2nKjnCqw6UB5oaH8XyNeBDj6bA_n1mjejzhFQUP3O1NfjFLHr3FOaeHcTOOT?auto=format&w=3840',
     address: '0xa852c97ea3a2d974ece086e7024ee281f246a959',
   },
   {
-    title: 'MyToken',
+    title: 'MyToken- Rare Apes',
     type: 'ERC721',
-    imgUrl: 'img/art-work/c1.png',
+    imgUrl: 'https://data.rareapepes.com/1.png',
     address: '0x3Bb4B7Bdd1e1C1948f4035E11084b08b5a80E0b2',
   },
   {
     title: 'Prophet',
     type: 'ERC721',
-    imgUrl: 'img/art-work/c1.png',
+    imgUrl:
+      'https://i.seadn.io/gae/JSbYG34rjQsC_7H7YzadR8dcRHko-VBbtw1bG-eR5kx_t7CocZ6CGKtMQPK5kuKXA9UFAm-_XjS9zWdbt2Y0lBhjZPZLVxUQ15m_Xg?auto=format&w=1000',
     address: '0xF7C6300DC134B8Ab82c6dbdE9d35e54b2E4Bfcfd',
-  },
-  {
-    title: 'Neutrons',
-    type: 'ERC20',
-    imgUrl: 'img/art-work/c2.png',
-    address: '0x85B2aeC8F0196AaeD22a03F4cEa9F3719A73f0fD',
   },
 ];
